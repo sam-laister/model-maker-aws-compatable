9b3156258d062f231633520d6d50bac1863d52c6
Sam <saml@everbit.dev>
2025-05-25 23:45:30 +0100

    Added app

--- app/database/database.go
+++ app/database/database.go
@@ -16,6 +16,7 @@ var DB *gorm.DB
 func ConnectDatabase() error {
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_TIMEZONE"))
 
+	fmt.Println("Connecting to database with DSN:", dsn)
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
